c675c78d50ec3d7fe4c0d22cb748f8a9c9b26f05
Mohamed Hassan <mhass405@gmail.com>
2021-04-07 19:29:36 +0000

    adding stdin as input from the user

--- play.js
+++ play.js
@@ -3,3 +3,22 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+  process.exit();
+  }
+};
+
+const setupInput = function(data) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+}
+setupInput();
+
