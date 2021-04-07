8e91a72e74ef3090c889ee2536a46135fdeea9e6
Mohamed Hassan <mhass405@gmail.com>
2021-04-07 19:35:32 +0000

    importing stdin from another file

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 // PORT: 50542
 // HOST: "135.23.222.131"
 const connect = require('./client');
+const setupInput = require('./input');
 console.log('Connecting ...');
 connect();
 
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-  process.exit();
-  }
-};
 
-const setupInput = function(data) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => {
-    handleUserInput(data);
-  });
-  return stdin;
-}
 setupInput();
 
